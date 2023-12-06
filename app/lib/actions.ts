ffffa01fb0d7c88bcdb68217c6745244260f9562
gongyuqi <gong_yu_qi@163.com>
2023-12-06 14:18:41 +0800

    complete learn course of nextjs

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -3,7 +3,9 @@
 import { z } from 'zod';
 import { sql } from '@vercel/postgres';
 import { revalidatePath } from 'next/cache';
-import { redirect } from 'next/navigation';
+import { redirect, useRouter } from 'next/navigation';
+import { signIn } from '@/auth';
+import { AuthError } from 'next-auth';
 
 const FormSchema = z.object({
   id: z.string(),
@@ -102,4 +104,26 @@ export async function deleteInvoice(id: string) {
       message: 'Database Error: Failed to Delete Invoice.',
     };
   }
+}
+
+
+export async function authenticate(prevState: string | undefined, formData: FormData) {
+  try {
+    const options = {
+      ...Object.fromEntries(formData),
+      redirect: false,
+    }
+    await signIn('credentials', options);
+    
+  } catch (error) {
+    if (error instanceof AuthError) {
+      switch (error.type) {
+        case 'CredentialsSignin':
+          return 'Invalid credentials.';
+        default:
+          return 'Something went wrong.';
+      }
+    }
+  }
+  redirect('/dashboard');
 }
